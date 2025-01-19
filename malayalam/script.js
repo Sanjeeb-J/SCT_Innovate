50cd863deca0a87d6eb205b393fefab2637406de
Sanjeeb J <sanjeebkodoth@gmail.com>
2025-01-19 07:55:14 +0530

    .

--- malayalam/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('english').addEventListener('click', function() {
-    window.location.href = 'english_page.html'; // Redirect to English page
-});
-
-document.getElementById('malayalam').addEventListener('click', function() {
-    window.location.href = 'malayalam_page.html'; // Redirect to Malayalam page
-});
